8bfa07dc9b16f14628ac960065c8ad6c411fb4b6
jolyeh <yehezekocolojoelprince@gmail.com>
2025-06-25 10:11:00 +0100

    ae

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import app from './app.js';
 
 const PORT = 3000;
-const HOST = "127.0.0.1";
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+/* const HOST = "127.0.0.1"; */
+app.listen(PORT, () => {
 });
